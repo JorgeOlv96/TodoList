598d3073a816bddb900b35fa7893cc3ba3256eb5
JorgeOlv96 <2020171010@uteq.edu.mx>
2024-04-14 15:00:32 -0600

    Agregar archivos de proyecto.

---
+++ TodoList/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using TodoList.Pages;
+
+namespace TodoList
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ToDoPage), typeof(ToDoPage));
+            Routing.RegisterRoute(nameof(RegistroTareaPage), typeof(RegistroTareaPage));
+            Routing.RegisterRoute(nameof(RegistroEncuestaPage), typeof(RegistroEncuestaPage));
+        }
+    }
+}
---
+++ TodoList/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using LocalizationResourceManager.Maui;
+
+namespace TodoList
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        ILocalizationResourceManager _localizationResourceManager;
+        LocalizedString counterClicked;
+
+
+        public MainPage(ILocalizationResourceManager localizationResourceManager)
+        {
+            InitializeComponent();
+            _localizationResourceManager = localizationResourceManager;
+            counterClicked = new(() => _localizationResourceManager["CounterClicked"]);
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            if (_localizationResourceManager.CurrentCulture.TwoLetterISOLanguageName == "nl")
+                _localizationResourceManager.CurrentCulture = new System.Globalization.CultureInfo("en");
+            else
+                _localizationResourceManager.CurrentCulture = new System.Globalization.CultureInfo("nl");
+
+        
+            count++;
+
+            CounterBtn.Text = string.Format(counterClicked.Localized, count);
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+    }
+
+}
